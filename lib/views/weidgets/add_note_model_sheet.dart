614903bb7e72c89e4295e5ec82b0be3322f936f9
eiad <eiadhany04@gmail.com>
2023-08-23 01:02:40 +0300

    create custom text field

--- lib/views/weidgets/add_note_model_sheet.dart
+++ lib/views/weidgets/add_note_model_sheet.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'custom_text_field.dart';
+
 class AddNoteModelSheet extends StatelessWidget {
   const AddNoteModelSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Column(
+      children: [
+        CustomTextField(),
+      ],
+    );
   }
 }
