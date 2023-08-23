aa6d200109c6a824397fc6c69563b3f32cfcbad8
eiad <eiadhany04@gmail.com>
2023-08-23 03:07:25 +0300

    add custom app bar to edit note view

--- lib/views/edit_notes_view_bodey.dart
+++ lib/views/edit_notes_view_bodey.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'weidgets/custom_app_bar.dart';
+
 class EditNotesViewBody extends StatelessWidget {
   const EditNotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 25,
+            ),
+            CustomAppBar(
+              title: 'Edit Note',
+              icon: Icons.check,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
